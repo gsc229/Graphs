1f4b228d4fd845c77e6e1fd0b044a57e4e784f1a
Greg <gsc229@gmail.com>
2020-03-20 12:10:49 -0400

    update_logs, update_travel_graph

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -35,21 +35,23 @@ traversal_path = []
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
-visited_rooms.add(player.current_room)
+visited_rooms.add(player.current_room.id)
 possible_exits = player.current_room.get_exits()
 print(f"{possible_exits}")
 
 travel_graph = {}
 
-def update_travel_graph(room_id, prev_room=None, prev_dir=None):
-    print(f"\nupdate_rooms_to_explore({room_id})")
-    travel_graph[room_id] = {}  
-    for direction in player.current_room.get_exits():  
-        room_dirs = travel_graph[room_id]
-        room_dirs.update({direction: '?'})
+def update_travel_graph(room_id, prev_or_next_room=None, prev_or_next_dir=None):
+    print(f"\nupdate_travel_graph({room_id})")
+    # initiate a dictionary for a NEW room to take directions:
+    if prev_or_next_room is None:
+        travel_graph[room_id] = {}  
+        for direction in player.current_room.get_exits():  
+            room_dirs = travel_graph[room_id]
+            room_dirs.update({direction: '?'})
         
-    if prev_room is not None:
-        travel_graph[room_id][prev_dir] = prev_room
+    if prev_or_next_room is not None:
+        travel_graph[room_id][prev_or_next_dir] = prev_or_next_room
         
     print(f"{travel_graph}\n")
 
@@ -64,10 +66,45 @@ def get_unexplored_dirs(room_id):
         random_num = random.randrange(0, len(unexplored))
         return unexplored[random_num]
 
+def update_logs(room_id, direction):
     
+    print(f"\nLogs Updated!!!")
+    # before swithcing to new room
+    # update the traversal path for the current room
+    old_room = player.current_room
+    room_in_next_direction = old_room.get_room_in_direction(direction).id
+    print(f"current_room: {old_room.id}, room_in_next_direction({direction}): {room_in_next_direction}")
+    travel_graph[old_room.id][direction] = room_in_next_direction
+    traversal_path.append(direction)
 
-while len(visited_rooms) < len(room_graph):
+    # find the opposite direction to updated direction of room coming from
+    opposite = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
 
+    player.travel(direction)
+    new_room = player.current_room
+    # after travel, check to see if the new room has been logged in the graph
+    if new_room.id not in travel_graph:
+        # if it hasn't initiate a new dictionary in the graph, then immediately add the direction,
+        # and room the player came from
+        update_travel_graph(new_room.id)
+        update_travel_graph(new_room.id, old_room.id, opposite[direction] )
+
+    # else if it has, update the direction with the room the player came from.
+    else:
+        update_travel_graph(new_room.id, old_room.id, opposite[direction])
+    
+    
+    
+       
+    print(f"traversal_path: {traversal_path}")
+    print(f"travel_graph: {travel_graph}\n")
+
+
+
+# ================ WHILE LOOP =================================
+count = 0
+while count < 2:
+    count += 1
     if player.current_room.id not in travel_graph:
         update_travel_graph(player.current_room.id)
 
@@ -78,11 +115,13 @@ while len(visited_rooms) < len(room_graph):
         # do sth
         print("NADA")
     else:
-        player.travel(unexplored_direction)
+        
         print(f"now in room {player.current_room.id}")
+        update_logs(player.current_room.id, unexplored_direction)
+        
 
 
-    break
+    
 
 
 for move in traversal_path:
